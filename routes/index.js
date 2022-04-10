77b9ed864672b60018f34cf71400fae11dc591b2
luisamlopez0 <lmlopez.19@est.ucab.edu.ve>
2022-04-10 18:09:48 -0400

    CRUD phones, fised hierarchy

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,20 @@ const productsRouter =require('./productsRoute')
 const salesRouter =require('./salesRoute')
 const clientsRouter =require('./clientsRoute');
 const brandsRouter =require('./brandsRoute');
-// const phonesRouter =require('./phonesRoute');
+ const phonesRouter =require('./phonesRoute');
 // const purchaseRouter =require('./purchaseRoute');
 // const locationRouter =require('./locationsRoute');
 
 function routerApi(app){
   const router=express.Router();
   app.use('/api/v1',router)
+  // router.use('/sales/purchase', purchaseRouter);
+  // router.use('/products/location', locationRouter);
   router.use('/products',productsRouter);
+  router.use('/clients/phones', phonesRouter);
   router.use('/clients',clientsRouter);
   router.use('/sales', salesRouter);
   router.use('/brands', brandsRouter);
-  // router.use('/clients/phones', phonesRouter);
-  // router.use('/sales/purchase', purchaseRouter);
-  // router.use('/products/location', locationRouter);
 }
 
 module.exports=routerApi;
