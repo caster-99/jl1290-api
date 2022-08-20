3910b9fbdb63f1e4d1c2f8803aeba0e6d80870a3
luisamlopez0 <lmlopez.19@est.ucab.edu.ve>
2022-08-19 23:37:45 -0400

    Add deploy config

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const routerApi =require('./routes');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const  { logErrors, errorHandler,boomErrorHandler } = require('./middlewares/errorHandler')
 
 app.use(express.json());
 
+const whiteList = ['http://localhost:8080','https://inventariojl1290.com'];
+const options={
+  origin:(origin, callback)=>{
+    if(whiteList.includes(origin)|| !origin){
+      callback(null,true);
+    }else{
+      callback(new Error('No permitido'));
+    }
+  }
+};
+app.use(cors(options));
+
+
 app.listen(port, () => {
   try {
-    // console.log('Port: ' + port);
-    // console.log("Conexión exitosa");
+    console.log('Port: ' + port);
+    console.log("Conexión exitosa");
   } catch (error) {
-    // console.log(error.message);
+    console.log(error.message);
   }
 
 });
