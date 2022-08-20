83347858b577b1dcdac56ab76e135fb42148b06b
Luisa López <lmlopez.19@est.ucab.edu.ve>
2022-08-19 22:15:41 -0400

    Fix errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,11 @@
-function logErrors (err, req, res, next) {
+function logErrors(err, req, res, next) {
+  console.log('logError');
   console.error(err);
   next(err);
 }
 
 function errorHandler(err, req, res, next) {
+  console.log('ErrorHandler');
   res.status(500).json({
     message: err.message,
     stack: err.stack,
@@ -18,5 +20,4 @@ function boomErrorHandler(err, req, res, next) {
   next(err);
 }
 
-
-module.exports = { logErrors, errorHandler, boomErrorHandler }
+module.exports = { logErrors, errorHandler, boomErrorHandler };
